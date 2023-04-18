c95bec6c2a0245fa4ff45aa75d2b4dfa29558d2b
Dustin Le <mewto3890@gmail.com>
2023-04-18 09:29:26 -0500

    testing first commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/honey", (req, res) => res.send("Don't try stealing my honey fool"));
 
 const PORT = 1337;
 
